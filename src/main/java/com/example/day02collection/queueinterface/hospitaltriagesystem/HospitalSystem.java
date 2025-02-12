d875c2b17b162ea98dc3f5d4c2eddc95425398fa
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-02-12 23:54:48 +0530

    [Aakrati] add. Implement Stack Using Queue

--- src/main/java/com/example/day02collection/queueinterface/hospitaltriagesystem/HospitalSystem.java
+++ src/main/java/com/example/day02collection/queueinterface/hospitaltriagesystem/HospitalSystem.java
@@ -1,4 +1,4 @@
 package com.example.day02collection.queueinterface.hospitaltriagesystem;
 
-public class HospitalTriageSystem {
+public class HospitalSystem {
 }
---
+++ src/main/java/com/example/day02collection/queueinterface/implementastackusingqueues/ImplementStackUsingQueues.java
@@ -0,0 +1,4 @@
+package com.example.day02collection.queueinterface.implementastackusingqueues;
+
+public class ImplementStackUsingQueues {
+}
