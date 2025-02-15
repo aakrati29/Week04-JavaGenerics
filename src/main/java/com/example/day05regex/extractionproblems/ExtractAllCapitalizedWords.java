dbd1544c107f926ce3023a1fbf6d641cf785bdf0
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-02-15 15:03:42 +0530

    [Aakrati] add . Extract all capitalized words form a sentence

---
+++ src/main/java/com/example/day05regex/extractionproblems/ExtractAllCapitalizedWords.java
@@ -0,0 +1,23 @@
+package com.example.day05regex.extractionproblems;
+
+import java.util.regex.Matcher;
+import  java.util.regex.Pattern;
+
+public class ExtractAllCapitalizedWords {
+
+    public static void extractCapitalWords(String input, String regex){
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(input);
+
+        while(matcher.find()){
+            System.out.println("Extracted capitalized word from a sentence : " + matcher.group());
+        }
+    }
+    public static void main(String[] args) {
+        String input = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+        String regex = "[A-Z][a-zA-Z]+";
+
+        extractCapitalWords(input,regex);
+    }
+}
