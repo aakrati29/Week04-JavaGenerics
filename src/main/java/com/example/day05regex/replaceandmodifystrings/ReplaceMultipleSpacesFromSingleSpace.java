5b5775b9e22a602d3e55fe06dc1570976eaa82c9
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-02-15 15:35:09 +0530

    [Aakrati] add . Replace multiple spaces from single space

---
+++ src/main/java/com/example/day05regex/replaceandmodifystrings/ReplaceMultipleSpacesFromSingleSpace.java
@@ -0,0 +1,19 @@
+package com.example.day05regex.replaceandmodifystrings;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReplaceMultipleSpacesFromSingleSpace {
+
+    private static void replace(String input, String regex) {
+        String result = input.replaceAll(regex, " ");
+        System.out.println("Result : " + result);
+    }
+
+    public static void main(String[] args) {
+        String input = "This is  an   example   with     multiple   spaces.";
+        String regex = "\\s+";
+
+        replace(input, regex);
+    }
+}
